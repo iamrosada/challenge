36e23d454c4a4bad89c31295ed1d744872676795
77-aguarosada <luisrosada2018@gmail.com>
2021-08-02 18:15:45 +0300

    this card is for favorite hero

--- heroes_challenge/src/components/card/index.js
+++ heroes_challenge/src/components/card/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import styles from './style.module.scss';
 import marvel from '../../assets/marvel.jpg';
+import styles from './style.module.scss';
 
 export default function Card() {
   return (
     <section className={styles.allCard}>
       <div>
         <img src={marvel} alt="hero" />
-        <span>Wanda maximoff</span>
-      </div>
-      <div>
-        <img src={marvel} alt="hero" />
-        <span>Wanda maximoff</span>
+        <div className={styles.removerHero}>
+          <span>ROSADA</span>
+          <button type="button">Remover</button>
+        </div>
       </div>
     </section>
   );
