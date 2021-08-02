fb9597f3bbb9c2927609af5ffcf7ff178abf967f
77-aguarosada <luisrosada2018@gmail.com>
2021-08-02 18:24:41 +0300

    updated HOME page

--- heroes_challenge/src/pages/Home/index.js
+++ heroes_challenge/src/pages/Home/index.js
@@ -1,20 +1,53 @@
-import React from 'react';
-import Card from '../../components/card';
+import React, { useState } from 'react';
+
 import Marvel from '../../components/marvel';
 import styles from './style.module.scss';
 
+import SearchBar from '../../components/searchBar';
+import SearchResults from '../../components/searchResults';
+
 export default function Home() {
+  const [searchText, setSearchText] = useState('');
+  const [superheroData, setSuperheroData] = useState([]);
+
+  async function searchSuperHeroes() {
+    const response = await fetch(
+      `https://www.superheroapi.com/api.php/10219177700206566/search/${searchText}`
+    );
+    const data = await response.json();
+    console.log('searchSuperHeroes -> data', data);
+
+    setSuperheroData(data.results);
+  }
+
+  function handleChange(e) {
+    const searchTerm = e.target.value;
+
+    setSearchText(searchTerm);
+    if (searchTerm.length === 0) {
+      setSuperheroData([]);
+    }
+    if (searchTerm.length > 3) {
+      searchSuperHeroes();
+    }
+  }
+
   return (
-    <section className={styles.homeContainer}>
-      <div className={styles.homeContent}>
-        <Marvel />
-      </div>
-      <div className={styles.card}>
-        <div className={styles.cardContent}>
-          <strong>FEATURED CHARACTERS</strong>
-          <Card />
+    <>
+      <section className={styles.homeContainer}>
+        <div className={styles.homeContent}>
+          <Marvel />
         </div>
-      </div>
-    </section>
+        <div className={styles.main}>
+          <SearchBar searchText={searchText} handleChange={handleChange} />
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardContent}>
+            <strong>FEATURED CHARACTERS</strong>
+            <SearchResults superheroData={superheroData} />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
