511ed8db72e417b49779250fe677dac7f13d8720
77-aguarosada <luisrosada2018@gmail.com>
2021-08-02 18:16:59 +0300

    added LINK in HEADER file

--- heroes_challenge/src/components/header/index.js
+++ heroes_challenge/src/components/header/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './style.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
-        <span>MY HEROES</span>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <Link to="/">
+          <span>MY HEROES</span>
+        </Link>
       </div>
     </header>
   );
