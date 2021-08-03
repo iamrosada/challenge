03546fb9d3dd1b9def29d8049e655a6684664f43
77-aguarosada <luisrosada2018@gmail.com>
2021-08-03 11:15:04 +0300

    function for remove item in Localstorage

--- heroes_challenge/src/pages/Favorites/index.js
+++ heroes_challenge/src/pages/Favorites/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../../components/card';
 import styles from './style.module.scss';
 
 export default function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+  useEffect(() => {
+    const getItem = localStorage.getItem('favorite');
+
+    if (getItem) {
+      setFavorites(JSON.parse(getItem));
+    }
+  }, []);
+
+  function handleRemove(id) {
+    const newFavorites = favorites.filter(favorite => favorite.id !== id);
+    localStorage.setItem('favorite', JSON.stringify(newFavorites));
+    setFavorites(newFavorites);
+  }
   return (
     <section className={styles.FavorContainer}>
       <div className={styles.FavorContent}>
         <span>FAVORITES PAGE</span>
-        <Card />
+        {favorites.map(favorite => (
+          <Card favorite={favorite} handleRemove={handleRemove} />
+        ))}
       </div>
     </section>
   );
